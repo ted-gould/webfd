53948872e80dfdf76810bc11fe6d714df23192c0
Pedro Coca <pedro.coca@canonical.com>
2015-08-14 14:59:04 +0100

    cv2 a manubrio

--- face_detect.py
+++ face_detect.py
@@ -2,9 +2,6 @@
 import sys
 import cv2
 
-# Modify the sys path to append the current directory
-sys.path.append('')
-
 # Get user supplied values
 cascPath = sys.argv[1]
 
---
+++ setup.py
@@ -0,0 +1,8 @@
+from distutils.core import setup
+from sys import *
+import cv2
+
+setup (name = "wfd",
+       version = "1.0",
+       py_modules = ['cv2'],
+      )
